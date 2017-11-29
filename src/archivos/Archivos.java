f2659595819e1d770b5b8c2b33b0695fed5a0067
Estudiante <Estudiante@lab0611>
2017-11-29 08:50:44 -0500

    Nuevo File

--- src/archivos/Archivos.java
+++ src/archivos/Archivos.java
@@ -6,6 +6,7 @@
 package archivos;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  *
@@ -14,19 +15,29 @@ import java.io.File;
 public class Archivos {
 
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
         File fichero=new File("C:\\Users\\Estudiante\\Desktop\\GUARDE AQUI SUS ARCHIVOS\\File");
         
         if(fichero.exists()==false)
         {   
            fichero.mkdir();//Creacion de carpeta con el nombre de la raiz
+           String Temp = "TEMP";
+            int i;
+           for(i=0;i<100;i++)
+             { 
+                /* File fichero=new File("C:\\Users\\Estudiante\\Desktop\\GUARDE AQUI SUS ARCHIVOS\\File"+);
+                 fichero.mkdir();//Creacion de carpeta con el nombre de la raiz*/
+                 fichero = File.createTempFile(Temp, ".docx" , new File("C:\\Users\\Estudiante\\Desktop\\GUARDE AQUI SUS ARCHIVOS\\File") );
+             }   
+           
         }
         else
         {   
             System.err.println("Existe la carpeta");
         }
           
-    }
+    
+        }
     
 }
